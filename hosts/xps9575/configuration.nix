c20273b1b61eb9234ee5660d98e990ea020685b4
swebra <eric@swebra.com>
2024-12-21 14:34:36 +0000

    Refactor laptop configuration

--- hosts/xps9575/configuration.nix
+++ hosts/xps9575/configuration.nix
@@ -1,34 +1,46 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../../nixos
+    ./hardware-configuration.nix
+  ];
+
+  # TODO: Abstract most of this
+
+  nixpkgs.config.allowUnfree = true;
+
+  users.users.${config.user} = {
+    isNormalUser = true;
+    description = config.user;
+    extraGroups = ["networkmanager" "wheel"];
+    packages = with pkgs; [
+      firefox
+      micro
+      vscode
+      dolphin-emu
     ];
+  };
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
+  # Networking
+  networking.networkmanager.enable = true;
+  networking.hostName = "xps9575"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  hardware.bluetooth.enable = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
-  # Set your time zone.
+  # Time and locale
   time.timeZone = "America/Edmonton";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_CA.UTF-8";
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -42,12 +54,6 @@
     layout = "us";
     xkbVariant = "";
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable bluetooth
-  hardware.bluetooth.enable = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
@@ -69,40 +75,9 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.swebra = {
-    isNormalUser = true;
-    description = "swebra";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    packages = with pkgs; [
-      firefox
-      kate
-    #  thunderbird
-    ];
-  };
-
   # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "swebra";
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  virtualisation.docker.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    alacritty
-    micro
-    vscode
-
-    dolphin-emu
-
-    # docker-compose
-  ];
+  services.xserver.displayManager.autoLogin.user = config.user;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -123,12 +98,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # Do not update for new release cycle, see NixOS docs
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
