c400235275901a9286965f1e4f822d8e585565a7
swebra <eric@swebra.com>
2025-01-08 20:40:31 +0000

    Update starship config

--- home-manager/shell/shell.nix
+++ home-manager/shell/shell.nix
@@ -11,11 +11,17 @@
       package.disabled = true;
       container.disabled = true;
 
+      python = {
+        format = "with [\${symbol}\${pyenv_prefix}(\${version} )]($style)";
+        # Only show when virtual environment is enabled
+        detect_extensions = [];
+        detect_files = [];
+      };
+
       nix_shell = {
-        # symbol = "❄️";
-        symbol = "";
+        format = "via [\${symbol}( \\($state\\))]($style) ";
+        symbol = "󱄅";
         impure_msg = "";
-        pure_msg = "";
       };
     };
   };
