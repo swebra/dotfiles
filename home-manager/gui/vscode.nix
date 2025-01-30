c8fae039981185c6064dd1bdbf6daf19f6780975
swebra <eric@swebra.com>
2025-01-30 03:47:36 +0000

    Add WIP vscode config

--- home-manager/gui/vscode.nix
+++ home-manager/gui/vscode.nix
@@ -1,5 +1,16 @@
-{...}: {
+{config, ...}: {
   programs.vscode = {
     enable = true;
   };
+
+  # TODO: make option for dotfiles clone location
+  xdg.configFile = let
+    parentDir = "${config.home.homeDirectory}/.dotfiles/home-manager/gui";
+    mkOutOfStoreSymlink = path: config.lib.file.mkOutOfStoreSymlink "${parentDir}/${path}";
+  in {
+    "Code/User/settings.json".source = mkOutOfStoreSymlink "vscode-config/settings.jsonc";
+    "Code/User/keybindings.json".source = mkOutOfStoreSymlink "vscode-config/keybindings.jsonc";
+  };
+
+  # TODO: Make a vscode-remote option that doesn't enable VSCode but handles extending the git config
 }
