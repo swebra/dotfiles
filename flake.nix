eb43d99396c49a93c047e3e1f711434f8b4c55a4
swebra <eric@swebra.com>
2025-03-30 21:33:58 -0600

    Add new build-two host

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
@@ -21,12 +22,19 @@
   outputs = {...} @ inputs:
     with (import ./myLib/make-outputs.nix inputs); {
       nixosConfigurations = {
+        # Temporarily build with unstable for mesa 25's 9070 XT support
+        build-two = inputs.nixpkgs-unstable.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {inherit inputs;};
+          modules = [./hosts/build-two/configuration.nix ./nixos];
+        };
         xps9575 = makeSystem "x86_64-linux" ./hosts/xps9575/configuration.nix;
         wsl = makeSystem "x86_64-linux" ./hosts/wsl/configuration.nix;
       };
 
       homeConfigurations = {
         default = makeHome "x86_64-linux" ./hosts/default/home.nix;
+        "eric@build-two" = makeHome "x86_64-linux" ./hosts/build-two/home.nix;
         "eric@xps9575" = makeHome "x86_64-linux" ./hosts/xps9575/home.nix;
         "eric@wsl" = makeHome "x86_64-linux" ./hosts/wsl/home.nix;
         "deck@steamdeck" = makeHome "x86_64-linux" ./hosts/steamdeck/home.nix;
