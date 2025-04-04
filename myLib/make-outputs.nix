1f6907cd6fba7e5d96373f99bad6c4c5e783d7be
swebra <eric@swebra.com>
2025-04-04 17:49:03 +0000

    Add pkgs-unstable reference in myLib/make-ouputs functions

--- myLib/make-outputs.nix
+++ myLib/make-outputs.nix
@@ -1,8 +1,21 @@
-{...} @ inputs: {
+{...} @ inputs: let
+  # Configure inputs.nixpkgs-unstable to allow unfree
+  getPkgsUnstable = arch:
+    import inputs.nixpkgs-unstable {
+      system = arch;
+      config.allowUnfree = true;
+    };
+  # nixpkgs configuration (as opposed to nixpkgs-unstable) is intentionally left to the nixos and
+  # home-manager modules such that legacyPackages can continue to be used for single evaluation
+  # https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462
+in {
   makeSystem = arch: configModule:
     inputs.nixpkgs.lib.nixosSystem {
       system = arch;
-      specialArgs = {inherit inputs;};
+      specialArgs = {
+        inherit inputs;
+        pkgs-unstable = getPkgsUnstable arch;
+      };
       modules = [configModule ../nixos];
     };
 
@@ -11,6 +24,7 @@
       pkgs = inputs.nixpkgs.legacyPackages.${arch};
       extraSpecialArgs = {
         inherit inputs;
+        pkgs-unstable = getPkgsUnstable arch;
         private = inputs.private;
       };
       modules = [configModule ../home-manager];
