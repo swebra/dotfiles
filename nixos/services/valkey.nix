ed9a38021e3c9c103e63dcedc0b303fa0b1ef770
swebra <eric@swebra.com>
2025-04-10 14:46:49 +0000

    Adjust valkey service

--- nixos/services/valkey.nix
+++ nixos/services/valkey.nix
@@ -2,6 +2,6 @@
   # See https://github.com/NixOS/nixpkgs/pull/315557 for upcoming changes
   services.redis = {
     package = pkgs.valkey;
-    servers.valkey.enable = true;
+    servers."".enable = true;
   };
 }
