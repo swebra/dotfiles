3373b2a8a074028092b4d2093eeb1fa65c9b6c93
swebra <eric@swebra.com>
2025-04-30 13:21:39 -0600

    Abstract KDE, KDE Connect

--- hosts/build-two/configuration.nix
+++ hosts/build-two/configuration.nix
@@ -42,10 +42,6 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
