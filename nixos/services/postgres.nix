f41d84cb233604331840e1cdd755e11d5c997491
swebra <eric@swebra.com>
2025-05-13 14:41:21 +0000

    Update postgres to v17

--- nixos/services/postgres.nix
+++ nixos/services/postgres.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   services.postgresql = {
     enable = true;
-    package = pkgs.postgresql_15;
+    package = pkgs.postgresql_17;
     extensions = ps: with ps; [postgis pg_repack];
 
     initialScript = pkgs.writeText "init-sql-script" ''
