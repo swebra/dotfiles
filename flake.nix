82899067d7dd3d5477c1cd3e8808a07de6dec0c4
swebra <eric@swebra.com>
2025-05-30 21:42:46 -0600

    Add the-cube

--- flake.nix
+++ flake.nix
@@ -33,6 +33,7 @@
           specialArgs = {inherit inputs;};
           modules = [./hosts/build-two/configuration.nix ./nixos];
         };
+        the-cube = makeSystem "x86_64-linux" ./hosts/the-cube/configuration.nix;
         xps9575 = makeSystem "x86_64-linux" ./hosts/xps9575/configuration.nix;
         wsl = makeSystem "x86_64-linux" ./hosts/wsl/configuration.nix;
       };
@@ -40,6 +41,7 @@
       homeConfigurations = {
         default = makeHome "x86_64-linux" ./hosts/default/home.nix;
         "eric@build-two" = makeHome "x86_64-linux" ./hosts/build-two/home.nix;
+        "eric@the-cube" = makeHome "x86_64-linux" ./hosts/the-cube/home.nix;
         "eric@xps9575" = makeHome "x86_64-linux" ./hosts/xps9575/home.nix;
         "eric@wsl" = makeHome "x86_64-linux" ./hosts/wsl/home.nix;
         "deck@steamdeck" = makeHome "x86_64-linux" ./hosts/steamdeck/home.nix;
