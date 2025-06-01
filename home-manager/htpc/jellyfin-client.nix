06a41a9f9adae9fba556171796048682981030eb
swebra <eric@swebra.com>
2025-06-01 15:18:55 -0600

    Replace torzu with eden, re-arrange emulation folder structure

--- home-manager/htpc/jellyfin-client.nix
+++ home-manager/htpc/jellyfin-client.nix
@@ -9,7 +9,7 @@
 
   # Tie into steam-rom-manager
   # TODO: Create steam-rom-manager module
-  home.file."Games/Emulation/roms/manifests/jellyfin.json".text = builtins.toJSON {
+  home.file."Games/Emulation/manifests/jellyfin.json".text = builtins.toJSON {
     title = "Jellyfin";
     target = "${config.home.homeDirectory}/.nix-profile/bin/jellyfinmediaplayer";
     launchOptions = "LD_PRELOAD= %command% --tv --fullscreen";
