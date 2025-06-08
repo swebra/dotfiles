cbfa69a709319eade1bf828c61f212b507558c6c
swebra <eric@swebra.com>
2025-06-08 17:44:47 -0600

    Update WIP htpc HDMI-CEC config

--- nixos/htpc/hdmi-cec.nix
+++ nixos/htpc/hdmi-cec.nix
@@ -1,26 +1,48 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  # TODO: echo from coreutils specifically?
+  cec-client = cmd: "echo '${cmd}' | ${lib.getExe' pkgs.libcec "cec-client"} -s";
+in {
   # https://github.com/Pulse-Eight/libcec/tree/master/systemd
-  systemd.services = {
-    cec-poweron = {
-      description = "Power on TV on startup";
-      script = "${pkgs.echo}/bin/echo 'on 0' | ${pkgs.libcec}/bin/cec-client -s";
-      #   serviceConfig = {
-      #     Type = "oneshot";
-      #     ExecStart = "/bin/echo 'standby 0' | /usr/bin/cec-client -s";
-      #     ExecStop = "/bin/echo 'standby 0' | /usr/bin/cec-client -s";
-      #   };
-      #   wantedBy = ["poweroff.target"];
-    };
-    # cec-poweroff = {
-    #   description = "Power off TV on ___";
-    #   serviceConfig = {
-    #     Type = "oneshot";
-    #     ExecStart = "/bin/echo 'standby 0' | /usr/bin/cec-client -s";
-    #     ExecStop = "/bin/echo 'standby 0' | /usr/bin/cec-client -s";
-    #   };
-    #   wantedBy = ["poweroff.target"];
-    # };
-  };
+  # https://github.com/josephbreiting/cec-systemd/
+  # https://github.com/NixOS/nixpkgs/blob/c736ff24eb207acb0d415be67edc7f9656ad2844/nixos/lib/systemd-unit-options.nix
 
   # Might be more that can be done https://wiki.archlinux.org/title/HDMI-CEC
+
+  # systemctl cat <service-name> to see the full output service file
+
+  # Use nix options instead of directly to get access to "full" shell with piping support
+  # Could also ExecStart=/bin/sh -c
+
+  systemd = {
+    timers.cec-boot = {
+      description = "Trigger cec-boot service on boot";
+      timerConfig = {
+        OnBootSec = 1;
+        OnStartupSec = 1;
+      };
+      wantedBy = ["timers.target"];
+    };
+
+    services = {
+      cec-boot = {
+        description = "Turns on TV and sets the input";
+        serviceConfig.Type = "oneshot";
+        preStart = cec-client "on 0"; # ExecStartPre
+        script = cec-client "as 0"; # ExecStart
+      };
+
+      cec-poweroff = {
+        description = "Turns off TV on shutdown";
+        serviceConfig.Type = "oneshot";
+        # idk why both
+        script = cec-client "standby 0"; # ExecStart
+        preStop = cec-client "standby 0"; # ExecStop
+        wantedBy = ["poweroff.target"];
+      };
+    };
+  };
 }
