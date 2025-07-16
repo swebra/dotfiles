58087af789cd3af38f701f15e7e70133eac9fe96
swebra <eric@swebra.com>
2025-07-16 08:58:28 -0600

    Add partition manager to kde config

--- nixos/gui/kde.nix
+++ nixos/gui/kde.nix
@@ -2,5 +2,8 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  programs.kdeconnect.enable = true;
+  programs = {
+    kdeconnect.enable = true;
+    partition-manager.enable = true;
+  };
 }
