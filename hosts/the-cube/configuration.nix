fd45c1dc02f37e109aab444feb00f76f95419daf
swebra <eric@swebra.com>
2025-09-25 07:13:15 -0600

    Remove rtl8821au kernal package from the cube

--- hosts/the-cube/configuration.nix
+++ hosts/the-cube/configuration.nix
@@ -36,7 +36,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.extraModulePackages = with config.boot.kernelPackages; [rtl8821au];
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
