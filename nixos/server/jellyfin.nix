5e81f2d07de4164f8a97fc9627c4fe7e649d16be
swebra <eric@swebra.com>
2025-10-07 21:14:32 -0600

    Add jellyfin reverse proxy to old server

---
+++ nixos/server/jellyfin.nix
@@ -0,0 +1,18 @@
+{private, ...}: {
+  # services.jellyfin = {
+  #   enable = true;
+  # };
+
+  services.caddy.virtualHosts."jellyfin.${private.server.domain}".extraConfig = ''
+    reverse_proxy http://192.168.1.100:8096
+  '';
+}
+# TODO:
+# - Actually host service here
+# - NAS mount
+# - Hardware transcoding
+# - Ultrachromic theme
+#   - https://github.com/matt1432/nixos-jellyfin
+#   - https://git.nelim.org/matt1432/nixos-configs/src/commit/29bc56e7492c5d2310016d5aed612a8fb4a5b127/devices/nos/modules/jellyfin/default.nix#L60
+# - Backup
+
