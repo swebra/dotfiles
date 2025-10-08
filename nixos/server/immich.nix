a623c0ae9bd068a97607fb8a59f87dfe74ca2759
swebra <eric@swebra.com>
2025-10-07 21:14:02 -0600

    Use proper URL for immich

--- nixos/server/immich.nix
+++ nixos/server/immich.nix
@@ -1,13 +1,22 @@
-{...}: {
+{
+  config,
+  private,
+  ...
+}: {
   # systemctl status immich-server
 
   services.immich = {
     enable = true;
-    host = "0.0.0.0"; # listen on IPv4, https://github.com/NixOS/nixpkgs/issues/446995
-    openFirewall = true; # TODO: move behind caddy
+    host = "127.0.0.1"; # listen on IPv4
   };
 
-  # services.caddy.virtualHosts."julia.internal".extraConfig = ''
-  #   reverse_proxy http://localhost:2283
-  # '';
+  services.caddy.virtualHosts."photos.${private.server.domain}".extraConfig = ''
+    reverse_proxy http://localhost:${builtins.toString config.services.immich.port}
+  '';
 }
+# TODO:
+# - NAS mount
+# - hardware transcoding
+# - backup and database dump settings
+# - email provider?
+
