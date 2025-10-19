ee2ff967e462ecbcdb5ecbfad990f873b9840a07
swebra <eric@swebra.com>
2025-10-18 21:23:11 -0600

    Add NFS mount options, use in immich

--- nixos/server/immich.nix
+++ nixos/server/immich.nix
@@ -1,21 +1,24 @@
-{config, ...}: {
+{config, ...}: let
+  immichDir = "/mnt/photos";
+in {
   # systemctl status immich-server
 
   services.immich = {
     enable = true;
     host = "127.0.0.1"; # listen on IPv4
+    mediaLocation = immichDir;
   };
 
-  # myOS.server.caddy.subdomain."photos" = ''
-  #   reverse_proxy http://localhost:${builtins.toString config.services.immich.port}
-  # '';
+  # immich user should get ownership of this by the immich tmpfiles settings
+  # https://github.com/NixOS/nixpkgs/blob/nixos-25.05/nixos/modules/services/web-apps/immich.nix#L415
+  myOS.server.nfs.mount."photos".to = immichDir;
+  systemd.services.immich-server.unitConfig.RequiresMountsFor = immichDir;
 
-  services.caddy.virtualHosts."julia.internal".extraConfig = ''
+  myOS.server.caddy.subdomain."photos" = ''
     reverse_proxy http://localhost:${builtins.toString config.services.immich.port}
   '';
 }
 # TODO:
-# - NAS mount
 # - hardware transcoding
 # - backup and database dump settings
 # - email provider?
---
+++ nixos/server/nfs.nix
@@ -0,0 +1,59 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  # https://wiki.nixos.org/wiki/NFS
+  # https://nixos.wiki/wiki/NFS
+
+  # Notes:
+  # - `fileSystems.<mount-point>` is creating an `/etc/fstab` entry, which is then
+  #   converted into a systemd mount unit by `systemd-fstab-generator`.
+  # - NFS mounts are implicitly dependent on network
+  #   - see the `_netdev` option in `man systemd.mount`
+  #   - see `systemctl list-dependencies <your-nfs-mount>.mount`
+
+  options = {
+    myOS.server.nfs = {
+      mount = lib.mkOption {
+        type = lib.types.attrsOf (lib.types.submodule {
+          options = {
+            to = lib.mkOption {
+              type = lib.types.str;
+              description = "Local mount point path.";
+            };
+          };
+        });
+        description = ''
+          Attribute set mapping of remote NFS paths to local mount point.
+
+          `nfs.<nfs volume name>.to = "<local mount point>";`
+        '';
+        default = {};
+      };
+
+      address = lib.mkOption {
+        type = lib.types.str;
+        description = "IP address of the NFS server (NAS) to mount from.";
+        default = "192.168.1.110";
+      };
+    };
+  };
+
+  config = {
+    boot.supportedFilesystems = ["nfs"];
+
+    # Create an NFS mount for each entry in config.myOS.server.nfs.mount
+    fileSystems =
+      lib.mapAttrs' (nfsVolume: mountAttrs: {
+        name = mountAttrs.to;
+        value = {
+          device = "${config.myOS.server.nfs.address}:/volume1/${nfsVolume}";
+          fsType = "nfs";
+          # TODO: NFS version?
+          # TODO: any mount options?
+        };
+      })
+      config.myOS.server.nfs.mount;
+  };
+}
