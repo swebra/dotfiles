5376f56cd204c011287e4cb426499433d16715a8
swebra <eric@swebra.com>
2025-11-07 07:50:39 -0700

    Rebuild julia-three on new hardware

--- hosts/julia-three/hardware-configuration.nix
+++ hosts/julia-three/hardware-configuration.nix
@@ -14,18 +14,18 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/abc8e469-2444-4bde-9c10-00d8abe6f75e";
+    { device = "/dev/disk/by-uuid/e9d8d363-c467-46dd-9db4-d9a20d0e5fc6";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/215D-1F99";
+    { device = "/dev/disk/by-uuid/E8C4-947D";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/6d9540ac-ffc0-4de0-a775-75170da1f4a8"; }
+    [ { device = "/dev/disk/by-uuid/4edfcf51-425f-4824-b206-93dcf8974e61"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -33,7 +33,9 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
