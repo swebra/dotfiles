27377b900b9b076ac03175eb4cc57009a4a0081a
swebra <eric@swebra.com>
2026-01-23 20:50:21 -0700

    Cleanup steam-gamescope, add auto-login option

--- nixos/htpc/steam-gamescope.nix
+++ nixos/htpc/steam-gamescope.nix
@@ -1,36 +1,53 @@
-{pkgs, ...}: let
-  sessionSwitcher = pkgs.writeShellScriptBin "steamos-session-select" ''
-    set -x
-    ${pkgs.pulseaudio}/bin/paplay ${pkgs.sound-theme-freedesktop}/share/sounds/freedesktop/stereo/service-logout.oga
-    echo "$(date): steamos-session-select called" >> /tmp/steamos-session-select.log
-    # sleep 5
-    # systemctl stop --user gamescope-session
-    steam -shutdown
-  '';
-in {
-  programs = {
-    gamescope.capSysNice = true;
-    steam = {
-      gamescopeSession = {
-        enable = true;
-        steamArgs = [
-          "-tenfoot"
-          "-pipewire-dmabuf"
-          # "-steamdeck"
-          "-steamos3" # Needed for steamos-session-select to be called
-        ];
-      };
-
-      # https://github.com/Jovian-Experiments/Jovian-NixOS/blob/development/pkgs/steam-jupiter/fhsenv.nix#L30
-      # https://linuxcommandlibrary.com/man/steamos-session-select
-      # Just make a script and add it here as an extraPackage? Or just generally on the path?
-      extraPackages = [
-        sessionSwitcher
-      ];
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options = {
+    myOS.htpc.steamGamescope.autoLogin = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = ''
+        Whether to enable auto-login into the Steam gamescope session.
+      '';
     };
   };
 
-  environment.systemPackages = [
-    sessionSwitcher
-  ];
+  config = {
+    programs = {
+      gamescope.capSysNice = true;
+      steam = {
+        gamescopeSession = {
+          enable = true;
+          steamArgs = [
+            "-tenfoot" # Big Picture mode
+            "-pipewire-dmabuf" # Default option
+            "-steamos3" # Needed for steamos-session-select to be called
+            # "-steamdeck"
+          ];
+        };
+
+        extraPackages = [
+          # Called by Steam when "switching to desktop mode"
+          # https://github.com/Jovian-Experiments/Jovian-NixOS/blob/development/pkgs/steam-jupiter/fhsenv.nix#L30
+          # https://github.com/ChimeraOS/gamescope-session-steam/blob/main/usr/bin/steamos-session-select
+          (pkgs.writeShellScriptBin "steamos-session-select" ''
+            echo "Shutting down gamescope session from custom steamos-session-select"
+            # Not currently using proper session, https://github.com/NixOS/nixpkgs/issues/419121
+            # systemctl stop --user gamescope-session
+            steam -shutdown
+          '')
+        ];
+      };
+    };
+
+    services.displayManager = lib.mkIf config.myOS.htpc.steamGamescope.autoLogin {
+      defaultSession = "steam";
+      autoLogin = {
+        enable = true;
+        user = config.myOS.user;
+      };
+    };
+  };
 }
