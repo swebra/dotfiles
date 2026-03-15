0fc51c6a88ceda7001a218ecacecdfb4fa7b503c
swebra <eric@swebra.com>
2026-03-15 08:57:24 -0600

    Update caddy cloudflare plugin

--- nixos/server/caddy.nix
+++ nixos/server/caddy.nix
@@ -35,8 +35,8 @@
       enable = true;
       # MANUAL UPDATE: Plugins are manually updated
       package = pkgs.caddy.withPlugins {
-        plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-        hash = "sha256-ea8PC/+SlPRdEVVF/I3c1CBprlVp1nrumKM5cMwJJ3U=";
+        plugins = ["github.com/caddy-dns/cloudflare@v0.2.3"];
+        hash = "sha256-bL1cpMvDogD/pdVxGA8CAMEXazWpFDBiGBxG83SmXLA=";
       };
 
       email = private.personal.email;
