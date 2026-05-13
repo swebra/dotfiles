5f1d810c67706b755f20d0a32e848aefddecc04c
swebra <eric@swebra.com>
2026-05-13 07:31:49 -0600

    Update controller docs

--- nixos/gaming/controllers.nix
+++ nixos/gaming/controllers.nix
@@ -1,7 +1,9 @@
 {pkgs, ...}: {
+  # steam-hardware (for Steam Controller) already enabled by steam itself
+
   # Support Xbox One wireless adapter
   hardware.xone.enable = true;
 
-  # Support dolphinbar and gamecube adapter
+  # Support dolphinbar and gamecube adapter (not automatic when installing through home-manager)
   services.udev.packages = [pkgs.dolphin-emu];
 }
