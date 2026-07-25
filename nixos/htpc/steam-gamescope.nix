c3fec1c2c94e578bc38ebadc2f972de5e84243d0
swebra <eric@swebra.com>
2026-07-25 14:18:59 -0600

    Update steam-gamescope comments/docs

--- nixos/htpc/steam-gamescope.nix
+++ nixos/htpc/steam-gamescope.nix
@@ -16,9 +16,12 @@
 
   config = {
     programs = {
-      # https://github.com/nixos/nixpkgs/issues/523427
-      # https://github.com/NixOS/nixpkgs/pull/524488
-      # https://github.com/nixos/nixpkgs/issues/523200
+      # Disabled, currently breaking steam in gamescope. See
+      # - https://github.com/NixOS/nixpkgs/pull/524488
+      # - https://github.com/NixOS/nixpkgs/issues/523427
+      # for the original, not-in-gamescope issue. Also possibly related:
+      # - https://github.com/NixOS/nixpkgs/issues/533140
+      # - https://github.com/nixos/nixpkgs/issues/523200
       # gamescope.capSysNice = true;
 
       steam = {
@@ -34,8 +37,12 @@
 
         extraPackages = [
           # Called by Steam when "switching to desktop mode"
-          # https://github.com/Jovian-Experiments/Jovian-NixOS/blob/development/pkgs/steam-jupiter/fhsenv.nix#L30
+          # https://github.com/Jovian-Experiments/Jovian-NixOS/blob/20d5e9195eee6692afe9186a9d45c79ba7aad56a/pkgs/steam-jupiter/fhsenv.nix#L30
           # https://github.com/ChimeraOS/gamescope-session-steam/blob/main/usr/bin/steamos-session-select
+          # WARN: This now looks to be depreciated for steamosctl, jovian has since
+          # moved over but maintains a stub:
+          # https://github.com/Jovian-Experiments/Jovian-NixOS/commit/52f140c07493f8bb6cd0773c7e1afe3e1fd1d1fa
+          # https://github.com/Jovian-Experiments/Jovian-NixOS/commit/31f540444f8a32555cf88260d660c9f4afe27bbb
           (pkgs.writeShellScriptBin "steamos-session-select" ''
             echo "Shutting down gamescope session from custom steamos-session-select"
             # Not currently using proper session, https://github.com/NixOS/nixpkgs/issues/419121
