f65cd5f4c2dfb0e89aa2e62feca6e5009933e298
swebra <eric@swebra.com>
2026-07-25 06:52:21 -0600

    Add silverbullet config

---
+++ nixos/server/silverbullet.nix
@@ -0,0 +1,25 @@
+{config, ...}: let
+  sbDir = "/mnt/notes";
+in {
+  services.silverbullet = {
+    enable = true;
+    listenPort = 8097;
+    spaceDir = sbDir;
+    # MANUAL SETUP: User auth is manually set (TODO: secret management)
+    # Contains `SB_USER=user:password`
+    envFile = "/etc/silverbullet/silverbullet.env";
+  };
+
+  myOS.server.nfs.mount."notes" = {
+    to = sbDir;
+    existsFor = {
+      services = ["silverbullet"];
+      user = config.services.silverbullet.user;
+      group = config.services.silverbullet.group;
+    };
+  };
+
+  myOS.server.caddy.subdomain."notes" = ''
+    reverse_proxy http://localhost:${builtins.toString config.services.silverbullet.listenPort}
+  '';
+}
