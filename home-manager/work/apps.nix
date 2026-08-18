c44d7364943f8423600f6f8c4e0b4f602debd251
swebra <eric@swebra.com>
2026-08-18 11:56:30 -0600

    Update home work config. Add onlyoffice font workaround

--- home-manager/work/apps.nix
+++ home-manager/work/apps.nix
@@ -1,7 +1,24 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     slack
-    libreoffice-qt # or libreoffice
-    onlyoffice-desktopeditors # just trialing
+    libreoffice # I don't love the QT breeze styling with libreoffice-qt
   ];
+
+  programs.onlyoffice.enable = true;
+  /*
+  Onlyoffice has font issues (https://github.com/ONLYOFFICE/DocumentServer/issues/1859)
+
+  Work around below based on
+  - https://github.com/ONLYOFFICE/DocumentServer/issues/1859#issuecomment-4484049376
+  - https://github.com/NixOS/nixpkgs/issues/373521#issuecomment-2588283507
+
+  Native work around to come in https://github.com/NixOS/nixpkgs/pull/526315
+  */
+  home.activation.copy-fonts-local-share = ''
+    mkdir -p ~/.local/share/fonts
+    cp -u ${pkgs.corefonts}/share/fonts/truetype/* ~/.local/share/fonts/
+    cp -u ${pkgs.vista-fonts}/share/fonts/truetype/* ~/.local/share/fonts/
+    chmod 544 ~/.local/share/fonts
+    chmod 444 ~/.local/share/fonts/*
+  '';
 }
